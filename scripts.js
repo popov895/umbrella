eca40be3d122b48901f7ed6dbcb9d2a330fcadbf
Eugene Popov <popov895@ukr.net>
2017-02-28 14:43:48 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll(".collapsible").forEach(function (element) {
+    document.querySelectorAll("#faq span").forEach(function (element) {
         element.addEventListener("click", function (event) {
             var div = event.target.nextElementSibling;
             if ("block" === div.style.display)
